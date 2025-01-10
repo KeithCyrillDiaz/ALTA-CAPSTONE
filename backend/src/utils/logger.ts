b60742210c7bf6dc2915a5dd05c1bbc984edb28e
keith.diazfacebook@gmaikl.com <keithcyrill.diaz@tup.edu.ph>
2025-01-10 17:03:57 +0800

    modified files

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -4,11 +4,11 @@ import { error } from "console"
 export const logger = {
     event: (message: string) => console.log("Event - ".magenta + message + "..."),
     error: (message: string, errorDetails?: any) => {
-        console.log("Error - ".red + message + "...");
+        console.log("Error - ".red + message);
         if(errorDetails) {
             console.log("Details: ".red + JSON.stringify(errorDetails, null, 2));
         }
     },
-    ready: (message: string) => console.log("Ready - ".green + message + "..."),
-    success: (message: string) => console.log("Event - ".yellow + message + "..."),
+    ready: (message: string) => console.log("Ready - ".green + message),
+    success: (message: string) => console.log("Event - ".yellow + message),
 }
