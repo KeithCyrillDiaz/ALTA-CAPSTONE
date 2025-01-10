948c760787eb9a55d76836eb848ae2e966495dd4
keith.diazfacebook@gmaikl.com <keithcyrill.diaz@tup.edu.ph>
2025-01-10 17:43:44 +0800

    added backend url from dotenv

--- backend/src/config/dotenv.ts
+++ backend/src/config/dotenv.ts
@@ -5,6 +5,7 @@ dotenv.config()
 export const configuration = {
     MongoDB_URL_Local: process.env.MONGODB_URI_LOCAL,
     MongoDB_URL_Atlas: process.env.MONGODB_URI_ATLAS,
+    backend_URL: process.env.BACKEND_URL,
     port: process.env.PORT || 3000,
     jwt_secret_user: process.env.JWT_SECRET_USER,
     jwt_secret_admin: process.env.JWT_SECRET_ADMIN,
