84de0c654ff4ba7537e08724527c084f9c5d03be
keith.diazfacebook@gmaikl.com <keithcyrill.diaz@tup.edu.ph>
2025-01-13 17:14:03 +0800

    added trycatch block for better error handling

--- backend/src/services/geminiAI/sendMessage.ts
+++ backend/src/services/geminiAI/sendMessage.ts
@@ -1,51 +1,58 @@
-
-
-import {GoogleGenerativeAI, HarmCategory, HarmBlockThreshold, SafetySetting} from '@google/generative-ai'
-import { geminiConfig } from '../../config/dotenv'
-
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold, SafetySetting } from '@google/generative-ai';
+import { geminiConfig } from '../../config/dotenv';
+import { logger } from '../../utils/logger';
 
 export const sendPromptToGemini = async (prompt: string) => {
-    
-    const modelName = geminiConfig.modelName;
-    const apiKey = geminiConfig.apiKey;
+    try {
+        const modelName = geminiConfig.modelName;
+        const apiKey = geminiConfig.apiKey;
 
-    const genAI = new GoogleGenerativeAI(apiKey);
-    const model = genAI.getGenerativeModel({model: modelName});
+        if(!apiKey || modelName) {
+            logger.error("gemini API_KEY and Model Name is not set in env");
+            return;
+        }
 
-    const generationConfig = {
-        temperature: 0.9,
-        topK: 1,
-        topP: 1,
-        maxOutputTokens: 2048
-    };
+        const genAI = new GoogleGenerativeAI(apiKey);
+        const model = genAI.getGenerativeModel({ model: modelName });
 
-    const safetySettings: SafetySetting[] = [
-        {
-            category:HarmCategory.HARM_CATEGORY_HARASSMENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-            category:HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-            category:HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-            category:HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-    ];
+        const generationConfig = {
+            temperature: 0.9,
+            topK: 1,
+            topP: 1,
+            maxOutputTokens: 2048
+        };
 
-    const chat = model.startChat({
-        generationConfig,
-        safetySettings,
-        history:[]
-    });
+        const safetySettings: SafetySetting[] = [
+            {
+                category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+                threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            },
+        ];
 
-    const result = await chat.sendMessage(prompt);
-    const response = result.response;
-    console.log("response: ", response.text())
+        const chat = model.startChat({
+            generationConfig,
+            safetySettings,
+            history: []
+        });
 
+        const result = await chat.sendMessage(prompt);
+        const response = result.response;
+
+        // console.log("response: ", response.text());
+
+    } catch (error) {
+        logger.error("Error sending prompt to Gemini:", error);
+    }
 }
