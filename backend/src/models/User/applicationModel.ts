49b58517bfcf0ae5298f25d923684d93fe71d0e1
keith.diazfacebook@gmaikl.com <keithcyrill.diaz@tup.edu.ph>
2025-01-13 16:20:18 +0800

    modified birthday field and remove reuqired on employment status and resume accuracy, added default value in employemnt status

--- backend/src/models/User/applicationModel.ts
+++ backend/src/models/User/applicationModel.ts
@@ -7,7 +7,7 @@ const applicationSchema = new mongoose.Schema ({
     // Personal Information
     givenName: {type: String, required: true},
     lastName: {type: String, required: true},
-    birhtDay: {type: Date, required: true},
+    birthday: {type: Date, required: true},
     gender: {type: String, required: true},
     email: {type: String, required: true},
     phoneNumber: {type: Number, required: true},
@@ -22,8 +22,8 @@ const applicationSchema = new mongoose.Schema ({
     workOnsite: {type: Boolean, required: true},
 
     //others
-    employmentStatus: {type: String, required: true},
-    resumeAccuracy: {type: Number, required: true}, //percentage result based on accuracy of resume in Job Decription from AI
+    employmentStatus: {type: String, default: "Pending"},
+    resumeAccuracy: {type: Number}, //percentage result based on accuracy of resume in Job Decription from AI
 
     //TimeStamp for filter
     month: {type: String, required: true},
