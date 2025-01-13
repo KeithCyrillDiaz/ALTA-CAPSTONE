f7e8c0b1f4c10d82652383a38885ef88476042a9
keith.diazfacebook@gmaikl.com <keithcyrill.diaz@tup.edu.ph>
2025-01-13 15:35:26 +0800

    removed  jwt_secret_admin encryptionKey and add  secretKey refresh_secret_key  access_secret_key

--- backend/src/config/dotenv.ts
+++ backend/src/config/dotenv.ts
@@ -7,10 +7,11 @@ export const configuration = {
     MongoDB_URL_Atlas: process.env.MONGODB_URI_ATLAS,
     backend_URL: process.env.BACKEND_URL,
     port: process.env.PORT || 3000,
-    jwt_secret_admin: process.env.JWT_SECRET_KEY_ADMIN,
+    refresh_secret_key: process.env.REFRESH_TOKEN_KEY,
+    access_secret_key: process.env.ACCESS_TOKEN_KEY,
     admin_pass: process.env.ADMIN_PASS,
     admin_user: process.env.ADMIN_USER,
-    encryptionKey: process.env.OWN_SECRET_KEY,
+    secretKey: process.env.SECRET_KEY,
     localOrigin: process.env.LOCAL_ORIGIN,
     globalOrigin: process.env.GLOBAL_ORIGIN
 }
