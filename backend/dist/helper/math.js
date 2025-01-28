05f758fb18ad920dbaf5bfe959d2dcc549c659ad
keith.diazfacebook@gmaikl.com <keithcyrill.diaz@tup.edu.ph>
2025-01-28 10:30:13 +0800

    update the dist folder to reflect on render

--- backend/dist/helper/math.js
+++ backend/dist/helper/math.js
@@ -2,8 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.caclucateIncreasePercentage = void 0;
 const caclucateIncreasePercentage = (previousValue, currentValue) => {
+    // CHECK IF PREV VALUE IS 0, IF YES RETURN 0
+    if (previousValue === 0)
+        return 0;
     const percentage = ((currentValue - previousValue) / previousValue) * 100;
-    return percentage;
+    ;
+    return Math.ceil(percentage);
 };
 exports.caclucateIncreasePercentage = caclucateIncreasePercentage;
 //# sourceMappingURL=math.js.map
